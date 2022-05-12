40052a6e3fe699bd5caa88822eda2f837f3b3977
johnwalters3 <jwalters@dev-10.com>
2022-05-12 11:12:24 -0500

    admin mode

---
+++ src/components/ShowAllGames.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShowAllGames = () => {
+  return (
+    <div>
+      <h3>Show All Games</h3>
+      <p>TODO: add GET request for all games</p>
+      <p>TODO: display game data</p>
+      <p>TODO: navigation for ADD, DELETE, UPDATE</p>
+    </div>
+  )
+}
+
+export default ShowAllGames
---
+++ src/components/ShowAllTeams.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShowAllTeams = () => {
+  return (
+    <div>
+      <h3>Show All Teams</h3>
+      <p>TODO: add GET request for all teams</p>
+      <p>TODO: display team data</p>
+      <p>TODO: navigation for ADD, DELETE, UPDATE</p>
+    </div>
+  )
+}
+
+export default ShowAllTeams
