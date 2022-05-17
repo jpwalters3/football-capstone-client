de36d8e7a528efbd4309b0c1e55d6359b7c2c3ed
johnwalters3 <jwalters@dev-10.com>
2022-05-17 14:59:39 -0500

    drop down menu

--- src/pages/Home/Statistics.js
+++ src/pages/Home/Statistics.js
@@ -6,9 +6,14 @@ const Statistics = () => {
     <div>
         <Header />
       <h3>Statistics page</h3>
-      <p>TODO: drop down menu of all statistical categories</p>
       <p>TODO: get request that calls the stored proceedure for the top ten players in the selected statistical category</p>
       <p>TODO: display response body</p>
+
+      <select>
+        <option value = "1">Goals</option>
+        <option value = "2">Assits</option>
+        <option value = "3">Clean Sheets</option>
+      </select>
     </div>
   )
 }
