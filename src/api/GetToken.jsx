40d7063ed52b03805e00dd786f2bf62156633838
Mabel Quek <mxquek1@gmail.com>
2022-05-17 15:19:01 -0500

    Working Login

--- src/api/GetToken.jsx
+++ src/api/GetToken.jsx
@@ -1,14 +1,9 @@
 //to be in app.js
 import React from "react";
 import {useState, useEffect} from "react";
-import { AdminContext } from '../App';
-import { TokenContext } from '../App';
 
-export async function GetToken(loginCredentials){
 
-    const [token, setToken] = React.useContext(TokenContext);
-    const [isAdmin, setIsAdmin] = React.useContext(AdminContext);
-
+export async function GetToken(loginCredentials, setToken, setIsAdmin){
     const init = {
         method: "POST",
         headers: {
@@ -21,13 +16,15 @@ export async function GetToken(loginCredentials){
     fetch("http://localhost:5101/api/auth/login", init)
         .then(response => {
             if(response.status !== 200){
-                alert(`Error ${response.status}: ${response.statusText}`);
-                // setToken("errorToken");
+                alert(`Error ${response.status}: ${response.statusText}\nViewing in Guest Mode`);
+                setToken("errorToken");
+                setIsAdmin(false);
                 return Promise.reject("error");
             }
             return response.json();
         })
         .then(json =>{
+            console.log(json);
             setToken(json);
             setIsAdmin(true);
         })
