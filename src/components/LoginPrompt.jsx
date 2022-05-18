bc3c8ad95d5c73327692f17837e65a3491816d15
Mabel Quek <mxquek1@gmail.com>
2022-05-18 16:26:26 -0500

    Formatted Login

--- src/components/LoginPrompt.jsx
+++ src/components/LoginPrompt.jsx
@@ -3,6 +3,8 @@ import {GetToken} from '../api/GetToken';
 import { Link } from 'react-router-dom'
 import { AdminContext } from '../App';
 import { TokenContext } from '../App';
+import Button from './Button';
+import '../styles/Form.css'
 
 const LoginPrompt = () => {
     const [token, setToken] = React.useContext(TokenContext);
@@ -19,16 +21,44 @@ const LoginPrompt = () => {
     }
     return(
         <div>
-            <form>
-                <label>Username: </label>
-                <input type = "text" value = {loginCredentials.userName} onChange = {(u) => {loginCredentialsSetter("userName", u.currentTarget.value)}}/> <br/>
-                <label>Password: </label>
-                <input type = "password" value = {loginCredentials.password} onChange = {(p) => {loginCredentialsSetter("password", p.currentTarget.value)}}/> <br/>
-                <Link to ="/Home"><button onClick = {()=>GetToken(loginCredentials, setToken, setIsAdmin)}>Log In</button></Link>
-                <br/>
-                <Link to ="/Home">Continue As Guest</Link>
-            </form>
-
+            <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm" id ="update-club">
+                <form>
+                    <div class="form-group mb-2">
+                        <label>Username: </label>
+                        <input type = "text" value = {loginCredentials.userName} onChange = {(u) => {loginCredentialsSetter("userName", u.currentTarget.value)}}
+                                class ="form-controll border border-solid border-gray-300"/> 
+                    </div>
+                    <div class="form-group mb-5">
+                        <label>Password: </label>
+                        <input type = "password" value = {loginCredentials.password} onChange = {(p) => {loginCredentialsSetter("password", p.currentTarget.value)}}
+                                class ="form-controll border border-solid border-gray-300"/>
+                    </div>
+                    <div class="form-group">
+                        <Link to ="/Home">
+                            <button onClick = {()=>GetToken(loginCredentials, setToken, setIsAdmin)} class="
+                                px-6
+                                py-2.5
+                                bg-blue-600
+                                text-white
+                                font-medium
+                                text-xs
+                                leading-tight
+                                uppercase
+                                rounded
+                                shadow-md
+                                hover:bg-blue-700 hover:shadow-lg
+                                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                                active:bg-blue-800 active:shadow-lg
+                                transition
+                                duration-150
+                                ease-in-out">Log In</button>
+                        </Link>
+                    </div>
+                </form>
+            </div>
+                    <Link to ="/Home"><button class = {"text-blue-600 hover:text-blue-700 hover:shadow-lg mt-2"} onClick = {()=>GetToken(loginCredentials, setToken, setIsAdmin)}>
+                        Continue As Guest
+                    </button></Link>
         </div>
     )
 }
